273ec1818343c106a4a18d2a6708a26825e1b764
victor121-tech <asama@thecasualtech.com>
2025-04-30 13:17:08 +0100

    homepage section

---
+++ components/Provider.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React, { use } from 'react'
+import { SessionProvider } from '@node_modules/next-auth/react'
+
+const Provider = ({ children, session }) => {
+  return (
+    <SessionProvider session={session}>
+      {children}
+    </SessionProvider>
+  )
+}
+
+export default Provider
